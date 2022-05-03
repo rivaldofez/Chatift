211637ade0be772a8e6f43e32feeb94d10527ab0
rivaldofez <rivaldofez@gmail.com>
2022-05-03 23:57:09 +0700

    create extension for rounded corner

---
+++ Chatift/Extension.swift
@@ -0,0 +1,26 @@
+//
+//  Extension.swift
+//  Chatift
+//
+//  Created by Rivaldo Fernandes on 03/05/22.
+//
+
+import SwiftUI
+
+// EXtension for rounded corners with specific corners
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+// Custom RoundedCorner shape
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
