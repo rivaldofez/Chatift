a94f167c706cb5e8d8e16cfb126d696ee627b7d1
rivaldofez <rivaldofez@gmail.com>
2022-05-03 21:54:36 +0700

    Initial Commit

---
+++ Chatift/ChatiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatiftApp.swift
+//  Chatift
+//
+//  Created by Rivaldo Fernandes on 03/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
