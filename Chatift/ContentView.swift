cd6f0eaddb5a9ca5b9fd428ef0f25ed3fbe7c1e3
rivaldofez <rivaldofez@gmail.com>
2022-05-03 23:51:47 +0700

    create dummy chats

--- Chatift/ContentView.swift
+++ Chatift/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    var messageArray = ["Hello you", "How Are you doing", "Iam building swiftui chat apps"]
+    
+    
     var body: some View {
         VStack {
             TitleRow()
+            
+            ScrollView {
+                ForEach(messageArray, id: \.self) { text in
+                    MessageBubble(message: Message(id: "12345", text: text, received: true, timestamp: Date()))
+                }
+            }
+            .padding(.top, 10)
+            .background(.white)
             
         }
         .background(Color("Peach"))
