385d528a6506eb381dfaccbfdce60d918de2f123
rivaldofez <rivaldofez@gmail.com>
2022-05-03 23:37:45 +0700

    create header page of chat

---
+++ Chatift/Components/TitleRow.swift
@@ -0,0 +1,50 @@
+//
+//  TitleRow.swift
+//  Chatift
+//
+//  Created by Rivaldo Fernandes on 03/05/22.
+//
+
+import SwiftUI
+
+struct TitleRow: View {
+    var imageUrl = URL(string: "https://images.unsplash.com/photo-1552234994-66ba234fd567?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80")
+    
+    var name = "Rivaldo Fernandes"
+    
+    var body: some View {
+        HStack(spacing: 20){
+            AsyncImage(url: imageUrl){image in
+                image.resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 50, height: 50)
+                    .cornerRadius(50)
+            }placeholder: {
+                ProgressView()
+            }
+            
+            VStack(alignment: .leading){
+                Text(name)
+                    .font(.title).bold()
+                
+                Text("Online")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image(systemName: "phone.fill")
+                .foregroundColor(.gray)
+                .padding(10)
+                .background(.white)
+                .cornerRadius(50)
+        }
+    }
+}
+
+struct TitleRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleRow()
+            .background(Color("Peach"))
+    }
+}
