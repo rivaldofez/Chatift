b837fb54d2bb9ba19644144139857b3e6bf8088e
rivaldofez <rivaldofez@gmail.com>
2022-05-03 23:48:34 +0700

    add timestamp on bubble chat

--- Chatift/Components/MessageBubble.swift
+++ Chatift/Components/MessageBubble.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MessageBubble: View {
     var message: Message
+    @State private var showTime = false
     
     var body: some View {
         VStack(alignment: message.received ? .leading : .trailing){
@@ -19,6 +20,16 @@ struct MessageBubble: View {
                     .cornerRadius(30)
             }
             .frame(maxWidth: 300, alignment: message.received ? .leading : .trailing)
+            .onTapGesture {
+                showTime.toggle()
+            }
+            
+            if(showTime) {
+                Text("\(message.timestamp.formatted(.dateTime.hour().minute()))")
+                    .font(.caption2)
+                    .foregroundColor(.gray)
+                    .padding(message.received ? .leading : .trailing, 25)
+            }
         }
         .frame(maxWidth: .infinity, alignment: message.received ? .leading : .trailing)
         .padding(message.received ? .leading : .trailing)
